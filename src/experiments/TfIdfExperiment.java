b61445c5293084a7c34c8d268ec2fed646617417
Srdjan <tarzanka@gmail.com>
2012-04-05 10:15:55 +0200

    slightly changed experiments

--- src/experiments/TfIdfExperiment.java
+++ src/experiments/TfIdfExperiment.java
@@ -19,9 +19,9 @@ public class TfIdfExperiment {
 
 	public static void main(String[] args) throws Exception {
 //......preprocessing		
-		Arguments.lowercase="n";
+		Arguments.lowercase="y";
 		Arguments.stopWordsRemoval="n";
-		Arguments.stemming="y";
+		Arguments.stemming="n";
 		Arguments.mergeLexicalVariants="n";
 //......matrix generation
 		Arguments.numberOfWordsInDocument=-1;//3,2,1
@@ -30,13 +30,13 @@ public class TfIdfExperiment {
 //......matrix type - normalizing frequencies
 		Arguments.matrixType = MATRIX_TYPE.TFIDF.ordinal();
 //......evaluation
-		Arguments.upBoarderForNumberOfMeanings = 6;
+		Arguments.upBoarderForNumberOfMeanings = 5;
 		Arguments.evaluationContextWindowSize = 3;
 		
 		Logger logger=Log.getLogger(Arguments.initLogName());
 		
 		logger.fine(Arguments.modelNameForLog());
-		long start = System.currentTimeMillis();
+		
 		
 //		PDT1Reader.cleanPDTFile();
 //		ExperimentPreprocessing.divideIntoTrainAndTestSets("pdt1_0/pdt1_cleaned");
@@ -52,9 +52,57 @@ public class TfIdfExperiment {
 		if(Arguments.stemming.equalsIgnoreCase("y"))LinguisticPreprocessing.stemCzechTerms();
 		
 //......train and test
+//		testDocumentSizeSentenceLevel(logger);
+		testDocumentSizeWordLevel(logger);
 		
-		for(int i=1; i<6; ){
+//......test on unseen data
+//		logger.fine("\t====test on unseen data====");
+//		ep.mergeFiles("pdt1_0//train", "pdt1_0//testDev", "pdt1_0//train+testDev");
+//		ci = new  CzechIndexer("pdt1_0//train+testDev", Arguments.numberOfSentencesInLuceneDoc, Arguments.numberOfWordsInDocument);
+//		ci.index("index");
+//		evaluator = new Evaluator(Arguments.upBoarderForNumberOfMeanings);
+//		evaluator.extractTestContext("pdt1_0//testFinal",Arguments.evaluationContextWindowSize);
+//		logger.fine(evaluator.testSetStatsForLog());
+//		evaluator.predict();
+//		logger.fine("Finished in "+(System.currentTimeMillis()-start)/1000+" seconds.");
+//		logger.fine(evaluator.evaluationStatsForLog());
+		
+	}
+	
+	public static void testDocumentSizeWordLevel(Logger logger) throws Exception{
+		long start = System.currentTimeMillis();
+		for(int i=1; i<4; ){
 			start = System.currentTimeMillis();
+			Arguments.numberOfWordsInDocument= i;
+			logger.fine(Arguments.preprocessingParamsForLog());
+			Evaluator evaluator = new Evaluator(Arguments.numberOfWordsInDocument,
+					Arguments.numberOfSentencesInLuceneDoc,
+					Arguments.upBoarderForNumberOfMeanings, "pdt1_0/train_");
+			evaluator.extractTestContext("pdt1_0/testDev_",Arguments.evaluationContextWindowSize);
+			logger.fine(evaluator.testSetStatsForLog());
+			
+			evaluator.predict();
+			
+			logger.fine(Arguments.evaluationParamsForLog());
+			ResultFormatter.precicisions.add(evaluator.getPrecision());
+			ResultFormatter.recalls.add(evaluator.getRecall());
+			ResultFormatter.Fmeasures.add(evaluator.getFMeasure(0.5f));
+			ResultFormatter.coverages.add(evaluator.getCoverage());
+			ResultFormatter.randomAcc=evaluator.getRandomPrecision();
+			
+			long end = (System.currentTimeMillis()-start)/1000;
+			logger.fine("Finished in "+end+" seconds.\n");
+			System.err.println(Arguments.initLogName()+" ["+end+"s]");
+			logger.fine(evaluator.evaluationStatsForLog());
+			i+=1;
+		}
+//		logger.fine(ResultFormatter.getTableForPreprocessingFC(Arguments.preprocessingName()));
+		logger.fine(ResultFormatter.getTableForPreprocessingPR(Arguments.preprocessingName()));
+	}
+	
+	public static void testDocumentSizeSentenceLevel(Logger logger) throws Exception{
+		for(int i=1; i<6; ){
+			long start = System.currentTimeMillis();
 			Arguments.numberOfSentencesInLuceneDoc= i;
 			logger.fine(Arguments.preprocessingParamsForLog());
 			Evaluator evaluator = new Evaluator(Arguments.numberOfWordsInDocument,
@@ -81,18 +129,6 @@ public class TfIdfExperiment {
 //		logger.fine(ResultFormatter.getTableForPreprocessingFC(Arguments.preprocessingName()));
 		logger.fine(ResultFormatter.getTableForPreprocessingPR(Arguments.preprocessingName()));
 
-//......test on unseen data
-//		logger.fine("\t====test on unseen data====");
-//		ep.mergeFiles("pdt1_0//train", "pdt1_0//testDev", "pdt1_0//train+testDev");
-//		ci = new  CzechIndexer("pdt1_0//train+testDev", Arguments.numberOfSentencesInLuceneDoc, Arguments.numberOfWordsInDocument);
-//		ci.index("index");
-//		evaluator = new Evaluator(Arguments.upBoarderForNumberOfMeanings);
-//		evaluator.extractTestContext("pdt1_0//testFinal",Arguments.evaluationContextWindowSize);
-//		logger.fine(evaluator.testSetStatsForLog());
-//		evaluator.predict();
-//		logger.fine("Finished in "+(System.currentTimeMillis()-start)/1000+" seconds.");
-//		logger.fine(evaluator.evaluationStatsForLog());
-		
 	}
 
 }
